ff655ed445586172851e0c290498f1ce383b5df6
Alexandre Barbier <a.barbier@epmistes.net>
2016-12-17 14:07:37 +0100

    feat(server): add CORS support

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ require('dotenv').config();
 const express = require('express');
 const lol = require('./lol');
 
-
 const app = express();
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'ranklol.com');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.get('/api/:summonerName', (req, res) => {
   const summonerName = req.params.summonerName;
