b6856c9416bfffd4a12d1aea1cc5fbb21c2dd5d0
Alexandre Barbier <a.barbier@epmistes.net>
2016-12-18 16:34:54 +0100

    chore(server): add CORS for dev

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const lol = require('./lol');
 
 const app = express();
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'ranklol.com');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
