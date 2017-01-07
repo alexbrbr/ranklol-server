8d59547dce4a70dc0daf6857c80d6ec4a5985ba2
Alexandre Barbier <a.barbier@epmistes.net>
2017-01-07 17:44:56 +0100

    chore(CORS): allow ranklol.com domain

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const dataGrouping = require('./data-grouping');
 const app = express();
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.header('Access-Control-Allow-Origin', 'http://ranklol.com');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
